eef99daf0c3dd08cf68ef854885b120d94725380
Toby Batch <tobias@neontribe.co.uk>
2025-08-01 20:39:28 +0100

    feat: loops over top sponsors

--- nrfc-theme/header.php
+++ nrfc-theme/header.php
@@ -11,32 +11,7 @@
   <div class="site-branding">
     <h1><a href="<?php echo esc_url(home_url('/')); ?>">Norwich Rugby Club</a></h1>
   </div>
-    <?php
-    $sponsors = new WP_Query([
-        'post_type' => 'sponsor',
-        'posts_per_page' => -1,
-        'orderby' => 'title',
-        'order' => 'ASC',
-    ]);
-
-    if ($sponsors->have_posts()) : ?>
-        <div class="header-sponsors">
-            <?php while ($sponsors->have_posts()) :
-                $sponsors->the_post(); ?>
-                <div class="sponsor-logo">
-                    <?php the_post_thumbnail('medium'); ?>
-                </div>
-                <?php
-                $sponsor_link = get_post_meta(get_the_ID(), '_sponsor_link', true);
-                if (!empty($sponsor_link)) :
-                    ?>
-                    <p><strong>Sponsor:</strong>
-                        <a href="<?php echo esc_url($sponsor_link); ?>" target="_blank">View Sponsor</a></p>
-                <?php endif; ?>
-            <?php endwhile; ?>
-            <?php wp_reset_postdata(); ?>
-        </div>
-    <?php endif; ?>
+  <?php get_template_part('template-parts/sponsors', 'frontpage'); ?>
   <nav>
     <?php wp_nav_menu(array('theme_location' => 'primary')); ?>
   </nav>
